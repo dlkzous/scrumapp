d567ffa16a0714602a72634aa6e53711fd18a79f
D'Souza, Kushal L <kushal.l.dsouza@accenture.com>
2015-09-18 23:07:58 +0100

    Display user profile if logged in

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose')
   , express = require('express')
   , router = express.Router()
-  , UserModel = mongoose.model('UserModel');
+  , UserModel = mongoose.model('UserModel')
+  , HttpStatus = require('http-status');
 
 // Users api
 router.route('/')
@@ -11,6 +12,20 @@ router.route('/')
         users: users
       });
     });
+  });
+
+router.route('/profile')
+  .get(function(req, res) {
+    if(req.session.auth) {
+      res.json({
+        user: req.user.toObject({getters: true})
+      });
+    } else {
+      res.status(HttpStatus.UNAUTHORIZED);
+      res.json({
+        message: 'You need to be logged in to view this information'
+      });
+    }
   });
 
 module.exports = router;
